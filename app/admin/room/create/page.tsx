4a7f6f9c32f142a8a7b5ae297d95951cfedb0f4f
Rerey <144551736+Rerey155-del@users.noreply.github.com>
2025-08-14 04:35:30 +0700

    oke

--- app/admin/room/create/page.tsx
+++ app/admin/room/create/page.tsx
@@ -3,7 +3,8 @@ import CreateForm from '@/components/admin/room/create-form'
 
 const CreateRoomPage = () => {
   return (
-    <div className='max-w-screen-xl px-4 py-16 mt-10 mx-auto'>
+    <div className='max-w-screen-xl px-4 py-16 mt-10 mx-auto '>
+     <h2 className="mb-6 font-bold text-xl">Create New Room</h2>
       <CreateForm />
     </div>
   )
