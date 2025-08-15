f98201ba13b1b97ea781380ae18c532583633be1
Rerey <144551736+Rerey155-del@users.noreply.github.com>
2025-08-15 09:38:23 +0700

    oke

--- app/admin/room/page.tsx
+++ app/admin/room/page.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import { prisma } from "@/lib/prisma"
 
-const RoomPage = () => {
+export default async function Page() {
+  const rooms = await prisma.room.findMany({
+    include: { RoomAmenities: true }
+  })
+
   return (
-    <div>
-      <h1>Room Management</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, aut? Maiores nisi cumque, quaerat illum omnis sapiente vitae dignissimos veniam ut. Maiores quod assumenda illum vel ipsum quaerat aspernatur ex. </p>
+    <div className="mb-12">
+      {rooms.map(room => (
+        <div key={room.id}>{room.name}</div>
+      ))}
     </div>
   )
 }
-
-export default RoomPage
