2a17c43533c86461a7d4aa5e5c91ca58b0b00747
Rerey <144551736+Rerey155-del@users.noreply.github.com>
2025-08-29 01:49:45 +0700

    edit data berdasarkan panggilan id

--- app/admin/room/edit/[id]/page.tsx
+++ app/admin/room/edit/[id]/page.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { notFound } from "next/navigation"
-import EditRoom from '@/components/admin/room/edit-room'
-import { Suspense } from 'react'
+import { Suspense } from "react";
+import { notFound } from "next/navigation";
+import EditRoom from "@/components/admin/room/edit-room";
+import { getRoomsById } from "@/lib/data";
 
-const UpdateRoomPage = async ({ params }: { params: Promise<{ id: string }> }) => {
-  const roomId = (await params).id;
-  if(!roomId) return notFound()
+const UpdateRoomPage = async ({ params }: { params: { id: string } }) => {
+  const { id } = params;
+  const room = await getRoomsById(id);
+
+  if (!room) return notFound();
+
   return (
-    <div className='max-w-screen-xl px-4 py-16 mt-10 mx-auto'>
-      <Suspense fallback={<p>loading....</p>}>
-        <EditRoom roomId={roomId} />
+    <div className="max-w-screen-xl px-4 py-16 mt-10 mx-auto">
+      <Suspense fallback={<p>Loading....</p>}>
+        <EditRoom roomId={room.id} /> 
       </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default UpdateRoomPage
+export default UpdateRoomPage;
